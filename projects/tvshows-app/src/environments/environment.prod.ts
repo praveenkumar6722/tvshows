bff96e54ff14fdd5352c6fc17d349105ac637c4d
praveen <>
2020-08-28 16:58:37 +0530

    update env

--- projects/tvshows-app/src/environments/environment.prod.ts
+++ projects/tvshows-app/src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
-import { Resource } from '@mylib/core';
-
-const endPoint = 'https://api.tvmaze.com';
-
 export const environment = {
-  production: true,
-  urls: {
-    [Resource.TvShows]: `${endPoint}/shows?page=0`,
-    [Resource.ShowDetails]: `${endPoint}/shows/`,
-    [Resource.Search]: `${endPoint}/search/shows/`,
-  }
+  production: true
 };
--- projects/tvshows-app/src/environments/environment.ts
+++ projects/tvshows-app/src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Resource } from '@mylib/core';
 const endPoint = 'https://api.tvmaze.com';
 
 export const environment = {
-  production: false,
+  production: true,
   urls: {
     [Resource.TvShows]: `${endPoint}/shows?page=0`,
     [Resource.ShowDetails]: `${endPoint}/shows/`,
